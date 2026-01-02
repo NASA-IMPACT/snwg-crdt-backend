dfd2895b7bd86c85a9c63f3fc33fd2242c9ff389
tnagorra <weathermist@gmail.com>
2026-01-02 10:33:14 +0545

    chore(env): make env variables non-mandatory
    - we do not need these variables when deploying in aws

--- app/utils/env.ts
+++ app/utils/env.ts
@@ -1,7 +1,7 @@
 import { cleanEnv, str, url } from "envalid";
 
 const env = cleanEnv(process.env, {
-    COGNITO_ISSUER: url(),
+    COGNITO_ISSUER: url({ default: undefined }),
 
     WEB_COGNITO_USER_POOL_ID: str(),
     WEB_COGNITO_USER_POOL_CLIENT_ID: str(),
@@ -9,7 +9,7 @@ const env = cleanEnv(process.env, {
     AWS_ACCESS_KEY_ID: str(),
     AWS_SECRET_ACCESS_KEY: str(),
     AWS_DEFAULT_REGION: str(),
-    AWS_RESOURCES_ENDPOINT: url(),
+    AWS_RESOURCES_ENDPOINT: url({ default: undefined }),
 
     S3_BUCKET: str(),
 
